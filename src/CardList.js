e72a425fc2692e3b742c2b54ae0d4b60d927665c
Sidharth Anand <sidharthanand67@gmail.com>
2022-08-28 16:37:58 +0530

    Complete project

---
+++ src/CardList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+    const cardArray = robots.map(robot => {
+        return (
+            <Card
+                key={robot.id}
+                id={robot.id}
+                name={robot.name}
+                email={robot.email}
+            />
+        );
+    }); 
+
+    return (
+        <div>
+            {cardArray}
+        </div>
+    );
+};
+
+export default CardList;
